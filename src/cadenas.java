16195cdac5242d3ecbf0b41a43c296509fafdf90
Neyzer Toledo <neyzertoledo@gmail.com>
2024-05-11 23:13:25 -0700

    Iniciar mi repositorio

---
+++ src/cadenas.java
@@ -0,0 +1,53 @@
+public class cadenas {
+    public static void main(String[] args) { 
+        /*String salto = "\n";
+
+        System.out.println("Hola\nMundo");
+
+        System.out.println("Hola\t Mundo");
+        System.out.println("H\t Mundo");
+        System.out.println("Holaaa\t Mundo");
+
+        System.out.println("Holaaa \"Mundo\"");
+        System.out.println("Holaaa \\Mundo\\");
+
+        System.out.println("Hola\b\bMundo");
+        
+
+        String name = "Polo Alejandro";
+        String[] ejemplo = name.split(",");
+
+        
+
+        System.out.println(name.length());
+        System.out.println(name.charAt(4));
+        System.out.println(name.contains("lo"));
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.trim());
+        System.out.println(name.replace("o","A"));
+
+        for (int i = 0; i < ejemplo.length; i++) {
+            System.out.println(ejemplo[i]);
+        }
+
+        System.out.println(String.format("Hola mi nombre es %s y tengo %d años y mido %f", 
+            name, 18, 1.70 ));
+        */
+
+        String texto = "Hola!";
+
+        System.out.println(texto);
+        for (int i = texto.length()-1; i >= 0 ; i--) {
+            System.out.print(texto.charAt(i));
+            
+        }
+
+
+        
+
+        
+
+
+    }
+}
