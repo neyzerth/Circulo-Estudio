3ecf8da9fa50d775f525a048481fd6efaab6c6f9
Neyzer Toledo <neyzertoledo@gmail.com>
2024-05-17 01:06:40 -0700

    Comentarios

--- src/Codigos/numericos.java
+++ src/Codigos/numericos.java
@@ -1,80 +1,113 @@
 package Codigos;
+
 import java.util.Random;
 
+//Manipular valores numericos y conversiones
 public class numericos {
     public static void main(String[] args) {
+        // Generar numeros al azar
+        System.out.println("Numeros al azar");
+        // MANERA 1 - crear instancia Random
         Random random = new Random();
-        System.out.println(random.nextInt(100)+1);
-        System.out.println(Math.random()*100);
-          
+        System.out.println(random.nextInt(50) + 1);
 
-        System.out.println("Funciones matematicas");
-        System.out.println(Math.pow(5,2));
-        System.out.println(Math.sqrt(25));
-        System.out.println(Math.abs(-25));
+        // MANERA 2 - Usar funcion de Math
+        // Imprime un numero al azar de 0.0 a 1.0, y se multiplica
+        // por el limite de numeros random (devuelve un double)
+        System.out.println((int) (Math.random() * 50) + 1);
 
-        System.out.println("Funciones redondeo");
-        System.out.println(Math.floor(2.3423432243432432));
-        System.out.println(Math.round(2.5));
-        System.out.println(Math.ceil(10.1));
+        System.out.println("\nFunciones matematicas");
+        System.out.println("pow(a,b)\t" + Math.pow(5, 2)); // potencia a^b
+        System.out.println("sqr(a)\t"   + Math.sqrt(25));   // raiz cuadrada de un numero
+        System.out.println("abs(a)\t"   + Math.abs(-25));   // retorna solo valor absoluto (positivo)
 
-        System.out.println("Funciones de comparacion");
-        System.out.println(Math.max(10,20));
-        System.out.println(Math.min(10,20));
+        System.out.println("\nFunciones de redondeo");
+        System.out.println("floor(a)\t" + Math.floor(2.3423432243432432)); // retorna solo el valor mas bajo (2.0)
+        System.out.println("round(a)\t" + Math.round(4.5)); // redondeaal valor mas cercano
+        System.out.println("ceil(a)\t"  + Math.ceil(10.1)); // retorna el valor mas arriba (11.0)
 
-        System.out.println("CONVERSIONES \n");
-        //-----------VALORES STRING
-        System.out.println("String");
+        System.out.println("\nFunciones de comparacion");
+        System.out.println("max(a,b)\t" + Math.max(10, 20)); // regresa el valor mayor entre a y b
+        System.out.println("min(a,b)\t" + Math.min(10, 20)); // regresa el valor menor entre a y b
+
+
+        // ----------- C O N V E R S I O N E S -------------------------------
+        System.out.println("\nCONVERSIONES \n");
+
+        // -----------VALORES STRING -------------------------------
+        System.out.println("\nValores String");
         String str;
-        
-        //Char
-        str = ""+'a';
-        System.out.println(str);
-         //Numericos
-        str = String.valueOf(6.6);
-         //Booleanos
-         str = String.valueOf(true);
 
-         //---------VALORES INT
-        System.out.println("int");
+        // Char
+        str = "" + 'a';
+        System.out.println("char ->\t" + str);
+
+        // Numericos
+        str = String.valueOf(6.6);
+        System.out.println("numericos ->\t" + str);
+
+        // Booleanos
+        str = String.valueOf(true);
+        System.out.println("booleanos ->\t" + str);
+
+
+
+        // ---------VALORES INT -------------------------------
+        System.out.println("\nValores int");
         int intNum;
 
-        //double, float
+        // double, float
         intNum = (int) 2.2;
-        System.out.println(intNum);
-        //String
-        intNum = Integer.parseInt("Pepito");
-        System.out.println(intNum);
-        //char
-        intNum = '2'-'0';
-        System.out.println(intNum);
+        System.out.println("real  ->\t"+intNum);
 
-        //--------VALORES DOUBLE
-        System.out.println("Double");
-        double doubNum;
-        //int
-        doubNum = (double) 10;
-        //String
-        doubNum = Double.parseDouble("34.6");
-        System.out.println(doubNum);
-        //char
+        // String
+        intNum = Integer.parseInt("2");
+        System.out.println("String ->\t"+intNum);
 
-        //System.out.println("Double");
-
-        boolean bool;
-        //String
-
-        //Hacer un programa que convierta una estatura de centimetros a metros
-        int heightCm = 172;
-        double heightM = 0;
-
-        System.out.println("Yo mido "+heightCm+"cm");
-        System.out.println("O tambien "+heightM+"M");
-
-        //Imprimir cualquier valor con decimal con solo 2 decimales
-        double decimal = 8.22946; 
-
+        // char
+        intNum = Character.getNumericValue('3');
+        System.out.println("char 1 ->\t"+intNum);
         
+        intNum = '2' - '0';
+        System.out.println("char 2 ->\t"+intNum);
 
+
+
+        // --------VALORES DOUBLE -------------------------------
+        System.out.println("\nValores Double");
+        double doubNum;
+
+        // int
+        doubNum = (double) 10;
+        System.out.println("int ->\t"+doubNum);
+
+        // String
+        doubNum = Double.parseDouble("34.6");
+        System.out.println("String ->\t"+doubNum);
+
+        // char
+        doubNum = Character.getNumericValue('8');
+        System.out.println("char ->\t"+doubNum);
+
+
+
+        // --------VALORES BOOLEAN -------------------------------
+        System.out.println("\nValores Boolean");
+        boolean bool;
+
+        // String
+        bool = Boolean.parseBoolean("true");
+        System.out.println("\"true\" ->\t"+bool);
+
+        bool = Boolean.parseBoolean("false");
+        System.out.println("\"false\" ->\t"+bool);
+
+        bool = Boolean.parseBoolean("TRUE");
+        System.out.println("\"TRUE\" ->\t"+bool);
+
+        bool = Boolean.parseBoolean("culquier otro texto nada q ver");
+        System.out.println("\"Otro\" ->\t"+bool);
+
+        System.out.println();
     }
 }
